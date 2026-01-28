bcfc166228ad12d89c8e1a9150e28cb43fbf5ef5
Mauro Ochoa <ochoa6719@gmail.com>
2026-01-28 18:10:33 -0500

    Quita titulo inicial

--- app.jsx
+++ app.jsx
@@ -46,7 +46,6 @@ function App() {
 
   return (
     <div style={{ padding: "20px", maxWidth: 600, margin: "auto" }}>
-      <h1>💰 Distribuidor de Valores</h1>
 
       <input
         placeholder="Nombre"
